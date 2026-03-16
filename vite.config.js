65196abe7293d94177f83dcd7c6a758a33b8af16
fahrikaymaz0 <fahrikaymaz0@gmail.com>
2026-03-16 19:27:25 +0300

    fix: add base path to vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: '/mycv/',
   plugins: [react()],
   server: {
     port: 3001,
